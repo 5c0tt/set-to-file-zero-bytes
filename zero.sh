f569acda750018898ddf08bcc46957e0d8183023
5c0tt <scott@newgeo.com>
2014-09-07 10:56:50 -0700

    Added a comment to explain the use of specific Mac OS X terminology

--- zero.sh
+++ zero.sh
@@ -5,4 +5,5 @@ set -o errexit  # Ignoring failing commands
 filename="$1"
 /bin/cat /dev/null > ${filename}
 
+# If you are not on Mac OS X comment this line out
 osascript -e 'display notification "Your file has been set to zero bytes in length" with title "Set File to Zero Bytes"'
